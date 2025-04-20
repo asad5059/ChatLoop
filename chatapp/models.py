6ecaa4d0310408b3516f1dc1353c620e78925327
asad5059 <asadpatiya3@gmail.com>
2025-04-20 23:04:59 +0600

    Add API and necessary models

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class ChatUsers(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(null=False, max_length=64, unique=True)
+    created_at = models.DateTimeField(default=datetime.now, null=False)
+    is_deleted = models.BooleanField(default=False)
+
+class ChatMessages(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(null=False, max_length=64)
+    message = models.CharField(max_length=512, null=False)
+    timestamp = models.DateTimeField(null=False, default=datetime.now)
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from chatapp.models import ChatMessages
+
+
+class MessagesAPIView(APIView):
+    
+    # TODO: We cannot relay on this API since anyone can read messages if they have the API endpoint
+    
+    def get(self, request):
+        """
+        Returns last 100 messages
+        """
+
+        messages = ChatMessages.objects.order_by('-timestamp')[:100][::-1]
+
+        return Response(data=messages, status=status.HTTP_200_OK)
+    
