1fd3a1fda0b6643cba510eaebff0dadd89bd3b32
asad5059 <asadpatiya3@gmail.com>
2025-04-27 23:43:29 +0600

    Add django templates for UI and render the templates on view

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import (
-    include,
-    path,
-)
-
-import chatapp.urls
+from django.urls import path
+from chatapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(chatapp.urls)),
+    path('', views.index, name="index"),
+    path('chat/<str:room_name>/', views.room, name="room"),
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -6,6 +6,20 @@ from rest_framework import status
 from chatapp.models import ChatMessages
 
 
+INDEX_HTML = 'index.html'
+ROOM_HTML = 'room.html'
+
+def index(request):
+    return render(request, INDEX_HTML)
+
+def room(request, room_name):
+    print(f'room name :::: {room_name}')
+    context = {
+        "room_name": room_name
+    }
+
+    return render(request, ROOM_HTML, context=context)
+
 class MessagesAPIView(APIView):
     
     # TODO: We cannot relay on this API since anyone can read messages if they have the API endpoint
